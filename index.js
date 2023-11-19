14312765339f940a3bbf3610be7cbb9a6244b25f
GNIPIEZI AURIOL <auriol.gnipiezi@viacesi.fr>
2023-11-19 17:07:21 +0100

    1

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+  const express = require('express');
+  const cors = require('cors');
+  const app = express();
+  const port = 3000;
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`API en écoute sur le port ${PORT}`);
+  });
+  // Activer CORS pour toutes les origines
+  app.use(cors());
+
+  // Importer les données JSON
+  const data = require('./data.json');
+
+app.get('/articles/:nom_article', (req, res) => {
+    console.log(req.params.nom_article);
+    const nomArticle = decodeURIComponent(req.params.nom_article.toLowerCase());
+    const article = data.find(item => item.Article.toLowerCase() === nomArticle);
+    console.log(article);
+
+    if (article) {
+      res.json(article);
+    } else {
+      res.status(404).send('Article non trouvé');
+    }
+});
+  // Route pour obtenir tous les articles
+  app.get('/articles', (req, res) => {
+    res.json(data);
+  });
+
+
+
+  // Démarrer le serveur
+  app.listen(port, () => {
+    console.log(`API en écoute sur http://localhost:${port}`);
+  });
